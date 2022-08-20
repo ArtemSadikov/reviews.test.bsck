364247a1c51d17824af508cb3b5b75dc2c84d61c
Marolboron <marolboron@ya.ru>
2022-08-20 14:31:33 +0300

    fix(girignore): added build to gitignore

--- build/src/modules/review/repository/review/review.interface-repository.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ReviewRepositorySymbol = void 0;
-exports.ReviewRepositorySymbol = Symbol.for('ReviewRepository');
-//# sourceMappingURL=review.interface-repository.js.map
